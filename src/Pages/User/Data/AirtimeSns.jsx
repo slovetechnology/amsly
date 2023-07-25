10b3118800a2b59c5ebb7362f54e203415d08bb0
slovetechnology <slovetech619@gmail.com>
2023-07-25 12:36:44 +0100

    fixing airtime sns

--- src/Pages/User/Data/AirtimeSns.jsx
+++ src/Pages/User/Data/AirtimeSns.jsx
@@ -1,55 +1,117 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserLayout from '../../../Components/User/UserLayout'
 import ContactToAdmin from '../ContactToAdmin'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import Loading from '/src/Components/General/Loading'
+import ConfirmAirtimePurchase from './Compos/ConfirmAirtimePurchase'
+import { ErrorAlert } from '/src/Components/Utils/Utility'
+import { Api, PostUrl } from '/src/Components/Utils/Apis'
+import { dispatchUser } from '/src/app/reducer'
 
 const AirtimeSns = () => {
-  return (
-    <UserLayout pagetitle="buy your airtime Sns">
-        <div className="mt-10">
-            <div className="bg-white w-full max-w-3xl p-5 shadow-xl mx-auto rounded-lg">
-                <form>
-                    <div className="mb-4">
-                        <div className="capitalize">Airtime Type</div>
-                        <select className="input">
-                            <option value="">--Select--</option>
-                            <option value="">AIRTIME SnS</option>
-                        </select>
-                    </div>
-                    <div className="mb-4">
-                        <div className="capitalize">Choose Network</div>
-                        <select className="input">
-                            <option value="">--Select--</option>
-                            <option value="">MTN 3%</option>
-                            <option value="">GLO 6%</option>
-                            <option value="">AIRTEL 5%</option>
-                            <option value="">ETISALAT 5%</option>
-                        </select>
-                    </div>
-                    <div className="mb-4">
-                        <div className="capitalize">recharge amount </div>
-                        <input type="number" className="input" />
-                    </div>
-                    <div className="mb-4">
-                        <div className="capitalize">mobile number </div>
-                        <input type="number" className="input" />
-                    </div>
-                    <div className="mb-4">
-                        <div className="grid grid-cols-2">
-                        <div className="capitalize">transaction pin  </div>
-                        <div className="text-right">Don't have! <Link to='/create_pin' className='text-indigo-600'>Create Pin</Link> </div>
+    const { subs } = useSelector(state => state.data)
+    const [mainsub, setMainsub] = useState({})
+    const dispatch = useDispatch()
+    const [loading, setLoading] = useState(false)
+    const [view, setView] = useState(false)
+    const [packs, setPacks] = useState([])
+    const [forms, setForms] = useState({
+        network: '',
+        amount: '',
+        mobile: '',
+        pin: '',
+        sub: ''
+    })
+    const handleForms = e => {
+        setForms({
+            ...forms,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleMainSub = (e) => {
+        const id = e.target.value
+        const findData = subs.find((item) => item.id?.toString() === id)
+        setMainsub(findData?.sub[0])
+        setForms({
+            ...forms,
+            sub: id,
+            network: findData?.sub[0]?.id
+        })
+    }
+
+    const ConfirmSubmission = e => {
+        e.preventDefault()
+        if (!forms.network) return ErrorAlert('Select your preferred network')
+        if (!forms.amount) return ErrorAlert('Subscription Amount required')
+        if (!forms.mobile) return ErrorAlert('Mobile Number required')
+        if (!forms.pin) return ErrorAlert('Airtime Pin required')
+        setView(!view)
+    }
+
+    const handleSubmission = async () => {
+        try {
+            const formdata = {
+                ...forms
+            }
+            setLoading(true)
+            const res = await PostUrl(Api.bills.airtime, formdata)
+            setLoading(false)
+            if (res.status === 200) {
+                SwalAlert('Request Successful', res.msg, 'success')
+                dispatch(dispatchUser(res.user))
+                setView(!view)
+                setTimeout(() => {
+                    navigate(0)
+                }, 2000);
+            } else {
+                ErrorAlert(res.msg);
+            }
+        } catch (error) {
+            return ErrorAlert(error)
+        }
+    }
+    return (
+        <UserLayout pagetitle="buy your airtime SNS">
+            {loading && <Loading />}
+            {view && <ConfirmAirtimePurchase handleSubmission={handleSubmission} closeView={() => setView(!view)} />}
+            <div className="mt-10">
+                <div className="bg-white w-full max-w-3xl p-5 shadow-xl mx-auto rounded-lg">
+                    <form onSubmit={ConfirmSubmission}>
+                        <div className="mb-4">
+                            <div className="capitalize">Choose Network</div>
+                            <select name="network" onChange={handleMainSub} className="input">
+                                <option value="">--Select--</option>
+                                {subs?.length > 0 && subs.map((item, i) => (
+                                   item.category.endsWith('-sns') && <option key={i} value={item.id}>{item.network.split(' ')[0]}</option>
+                                ))}
+                            </select>
                         </div>
-                        <input type="text" className="input" />
-                    </div>
-                    <div className="w-fit ml-auto">
-                        <button className="bg-indigo-600 capitalize rounded-full py-3 px-8 text-white">make payment</button>
-                    </div>
-                    <ContactToAdmin />
-                </form>
+                        <div className="mb-4">
+                            <div className="capitalize">recharge amount </div>
+                            <input name="amount" value={forms.amount} onChange={handleForms} type="number" className="input" />
+                        </div>
+                        <div className="mb-4">
+                            <div className="capitalize">mobile number </div>
+                            <input name="mobile" value={forms.mobile} onChange={handleForms} type="number" className="input" />
+                        </div>
+                        <div className="mb-4">
+                            <div className="grid grid-cols-2">
+                                <div className="capitalize">transaction pin  </div>
+                                <div className="text-right">Don't have! <Link to='/create_pin' className='text-indigo-600'>Create Pin</Link> </div>
+                            </div>
+                            <input name="pin" value={forms.pin} maxLength={4} onChange={handleForms} type="text" className="input" />
+                        </div>
+                        <div className="w-fit ml-auto">
+                            <button className="bg-indigo-600 capitalize rounded-full py-3 px-8 text-white">make payment</button>
+                        </div>
+                        <ContactToAdmin />
+                    </form>
+                </div>
             </div>
-        </div>
-    </UserLayout>
-  )
+        </UserLayout>
+    )
 }
 
 export default AirtimeSns
